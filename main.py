dbee70bfdba103c29058d6a7f7ea7d3cf3837ed3
Sommos <Sommosgaming@gmail.com>
2023-07-12 06:43:57 +0100

    added ability to retrieve top 10 songs of designated artist

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
-from requests import post
+from requests import post, get
 import base64
 import json
 import os
 
+# load environment variables
 load_dotenv()
-
+# get environment variables and assign to variables
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
@@ -34,4 +35,48 @@ def get_token():
 
     return token
 
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+def search_for_artist(token, artist_name):
+    # setup URL and headers for GET request
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    # encode artist_name to url format
+    query = f"?q={artist_name}&type=artist&limit=1"
+    # combine url and query
+    query_url = url + query
+    # send GET request
+    result = get(query_url, headers=headers)
+    #  convert json to python dictionary
+    json_result = json.loads(result.content)["artists"]["items"]
+    # check if artist was found
+    if len(json_result) == 0:
+        print("No artist found")
+        return None
+    else: 
+        return json_result[0]
+
+def get_songs_by_artist(token, artist_id):
+    # setup URL and headers for GET request
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US" 
+    headers = get_auth_header(token)
+    # send GET request
+    result = get(url, headers=headers)
+    #  convert json to python dictionary
+    json_result = json.loads(result.content)["tracks"]
+    # check if tracks was found
+    if len(json_result) == 0:
+        print("No tracks found")
+        return None
+    else: 
+        return json_result
+
 token = get_token()
+result = search_for_artist(token, "Polyphia")
+artist_id = result["id"]
+songs = get_songs_by_artist(token, artist_id)
+
+# enumerate through top 10 songs and print names
+for i, song in enumerate(songs):
+    print(f"{i+1}. {song['name']}")
