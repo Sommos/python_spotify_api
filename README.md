c7a82c328fa93e23fa9ff01e4c37368164adbdb9
Sommos <Sommosgaming@gmail.com>
2023-07-12 08:39:02 +0100

    added error handling, input validation and sanitization, loading spinner and additioanl artist information

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Python Spotify API
 
 <p align="center">
-    <img src="image0.png" width="400">
-    <img src="image1.png" width="400">
+    <img src="image0.png" height="300" width="400">
+    <img src="image1.png" height="300" width="400">
 </p>
 
 This program is to test the Spotify API, and retrieval of data from it.
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import sys
+from requests import RequestException
 from dotenv import load_dotenv
 from requests import post, get
 import base64
 import urllib
+import time
 import json
 import os
 import re
@@ -50,7 +53,7 @@ def search_for_artist(token, artist_name):
     query_url = url + query
     # send GET request
     result = get(query_url, headers=headers)
-    #  convert json to python dictionary
+    # convert json to python dictionary
     json_result = json.loads(result.content)["artists"]["items"]
     # check if artist was found
     if len(json_result) == 0:
@@ -65,7 +68,7 @@ def get_songs_by_artist(token, artist_id):
     headers = get_auth_header(token)
     # send GET request
     result = get(url, headers=headers)
-    #  convert json to python dictionary
+    # convert json to python dictionary
     json_result = json.loads(result.content)["tracks"]
     # check if tracks was found
     if len(json_result) == 0:
@@ -80,7 +83,7 @@ def get_albums_by_artist(token, artist_id):
     headers = get_auth_header(token)
     # send GET request
     result = get(url, headers=headers)
-    #  convert json to python dictionary
+    # convert json to python dictionary
     json_result = json.loads(result.content)["items"]
     # check if albums was found
     if len(json_result) == 0:
@@ -88,53 +91,122 @@ def get_albums_by_artist(token, artist_id):
         return None
     else: 
         return json_result
+    
+def get_song_info(token, song_id):
+    # setup URL and headers for GET request
+    url = f"https://api.spotify.com/v1/tracks/{song_id}" 
+    headers = get_auth_header(token)
+    # send GET request
+    result = get(url, headers=headers)
+    # convert json to python dictionary
+    json_result = json.loads(result.content)
+    return json_result
+
+def get_album_info(token, album_id):
+    # setup URL and headers for GET request
+    url = f"https://api.spotify.com/v1/albums/{album_id}" 
+    headers = get_auth_header(token)
+    # send GET request
+    result = get(url, headers=headers)
+    # convert json to python dictionary
+    json_result = json.loads(result.content)
+    return json_result
 
 def sanitize_filename(filename):
     # remove special characters from filename
     sanitized_filename = re.sub(r"[^\w\s.-]", "_", filename)
     return sanitized_filename
 
+def display_loading_spinner():
+    while True:
+        for cursor in "|/-\\":
+            print(f"\rLoading... {cursor}", end="")
+            sys.stdout.flush()
+            time.sleep(0.1)
+            # clear the line
+            print("\r\033[K", end="")
+        break
+
 token = get_token()
-user_artist = input("Enter an artist: ")
-result = search_for_artist(token, user_artist)
 
-# check if artist was found
-if result:
-    artist_id = result["id"]
-    songs = get_songs_by_artist(token, artist_id)
-    albums = get_albums_by_artist(token, artist_id)
-    # replace spaces with underscores
-    artist_folder = user_artist.replace(" ", "_")
-    # create folders for images
-    os.makedirs(f"images/{artist_folder}/songs/", exist_ok=True)
-    os.makedirs(f"images/{artist_folder}/albums/", exist_ok=True)
-    # enumerate through top 10 songs and print names
-    print("Top 10 songs:")
-    for i, song in enumerate(songs):
-        print(f"{i+1}. {song['name']}")
-        # retrieve the image URL of the song
-        image_url = song['album']['images'][0]['url']
-        # generate a filename for the image
-        filename = sanitize_filename(f"{song['name']}.png")
-        # set the filepath
-        filepath = os.path.join("images", artist_folder, "songs", filename)
-        # save the image
-        urllib.request.urlretrieve(image_url, filepath)
+# get user input, check if input is empty
+while True:
+    user_artist = input("Enter an artist: ")
+    if user_artist.strip():
+        break
+    print("Invalid artist name. Please try again.")
 
-    # add a break between songs and albums
-    print("\n" + "-"*20 + "\n")
+try:
+    result = search_for_artist(token, user_artist)
+    # check if artist was found
+    if result:
+        artist_id = result["id"]
+        songs = get_songs_by_artist(token, artist_id)
+        albums = get_albums_by_artist(token, artist_id)
+        # replace spaces with underscores
+        artist_folder = user_artist.replace(" ", "_")
+        # create folders for information
+        os.makedirs(f"info/{artist_folder}/songs/info", exist_ok=True)
+        os.makedirs(f"info/{artist_folder}/albums/info", exist_ok=True)
+        os.makedirs(f"info/{artist_folder}/songs/images", exist_ok=True)
+        os.makedirs(f"info/{artist_folder}/albums/images", exist_ok=True)
+        # enumerate through top 10 songs and print names
+        print("Top 10 songs:")
+        for i, song in enumerate(songs):
+            print(f"{i+1}. {song['name']}")
+            try:
+                # retrieve the image URL of the song
+                image_url = song['album']['images'][0]['url']
+                # generate a filename for the image
+                filename = sanitize_filename(f"{song['name']}.png")
+                # set the filepath
+                filepath = os.path.join("info", artist_folder, "songs", "images", filename)
+                # display loading spinner
+                display_loading_spinner()
+                # clear the line after loading spinner
+                print("\r\033[K", end="")
+                # save the image
+                urllib.request.urlretrieve(image_url, filepath)
 
-    # enumerate through top 10 albums and print names
-    print("Top 10 albums:")
-    for x, album in enumerate(albums[:10]):
-        print(f"{x+1}. {album['name']}")
-        # retrieve the image URL of the album
-        image_url = album['images'][0]['url']
-        # generate a filename for the image
-        filename = sanitize_filename(f"{album['name']}.png")
-        # set the filepath
-        filepath = os.path.join("images", artist_folder, "albums", filename)
-        # save the image
-        urllib.request.urlretrieve(image_url, filepath)
-else:
-    print("Artist not found.")
+                # get additional information about the song
+                song_info = get_song_info(token, song['id'])
+                # save the song information to a file
+                song_info_filepath = os.path.join("info", artist_folder, "songs", "info", f"{song['name']}.txt")
+                with open(song_info_filepath, "w") as file:
+                    file.write(json.dumps(song_info, indent=4))
+            except(urllib.error.URLError, urllib.error.HTTPError, IOError, RequestException) as e:
+                print(f"Error saving image for song '{song['name']}': {str(e)}")
+
+        # add a break between songs and albums
+        print("\n" + "-"*20 + "\n")
+
+        # enumerate through top 10 albums and print names
+        print("Top 10 albums:")
+        for x, album in enumerate(albums[:10]):
+            print(f"{x+1}. {album['name']}")
+            try:
+                # retrieve the image URL of the album
+                image_url = album['images'][0]['url']
+                # generate a filename for the image
+                filename = sanitize_filename(f"{album['name']}.png")
+                # set the filepath
+                filepath = os.path.join("info", artist_folder, "albums", "images", filename)
+                # display loading spinner
+                display_loading_spinner()
+                # clear the line after loading spinner
+                print("\r\033[K", end="")
+                # save the image
+                urllib.request.urlretrieve(image_url, filepath)
+
+                # get additional information about the album
+                album_info = get_song_info(token, album['id'])
+                # save the album information to a file
+                album_info_filepath = os.path.join("info", artist_folder, "albums", "info", f"{album['name']}.txt")
+                with open(album_info_filepath, "w") as file:
+                    file.write(json.dumps(album_info, indent=4))
+            except(urllib.error.URLError, urllib.error.HTTPError, IOError, RequestException) as e:
+                print(f"Error saving image for album '{album['name']}': {str(e)}")
+    else:
+        print("Artist not found.")
+except(urllib.error.URLError, urllib.error.HTTPError, IOError, RequestException) as e:
+    print(f"An error occurred during the request: {str(e)}")
